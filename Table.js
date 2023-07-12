62f98876cb743507e140bf8ef04bdaba053dee74
Gustavo Luis Schmaedecke <97gustavo.luis@gmail.com>
2023-07-12 20:48:43 -0300

    Projeto finaliado

---
+++ Table.js
@@ -0,0 +1,13 @@
+class Table {
+  constructor(arr) {
+    this.header = arr[0];
+    arr.shift();
+    this.rows = arr;
+  }
+
+  get RowCount() {
+    return this.rows.length;
+  }
+}
+
+module.exports = Table;
